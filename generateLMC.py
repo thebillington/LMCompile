c2b8dd6e7db4717a6d3d218023321f3b32b51b1f
Billy Rebecchi <billy.rebecchi@googlemail.com>
2019-03-01 17:47:28 +0000

    Add lexing for single characters

--- generateLMC.py
+++ generateLMC.py
@@ -1,5 +1,6 @@
 # Import the os module
 import os
+from tokens import *
 
 # Create a function to lex a given file
 def lex(fName):
@@ -10,8 +11,55 @@ def lex(fName):
         return
 
     # Fetch the data from the file
-    data = open(fName,"r").read()
-    print(data)
+    data = open(fName,"r").read() + "\0"
+
+    # Create a list to hold the tokens
+    tokens = []
+
+    # Count the number of tokens
+    count = -1
+
+    # Set the start position
+    pos = 0
+
+    # Create a loop to look over the file
+    while True:
+
+        # Check for end of file
+        if data[pos] == "\0":
+            break
+
+        # Check if the position in the string is a one character token
+        if data[pos] == " ":
+            tokens.append([SPACE, " ", pos])
+            pos += 1
+            continue
+        elif data[pos] == "\n":
+            tokens.append([LINEFEED, "\\n", pos])
+            pos += 1
+            continue
+        elif data[pos] == "\r":
+            tokens.append([CARRIAGERETURN, "\\r", pos])
+            pos += 1
+            continue
+        elif data[pos] == "\t":
+            tokens.append([TAB, "\\t", pos])
+            pos += 1
+            continue
+        elif data[pos] == ";":
+            tokens.append([SEMICOLON, ";", pos])
+            pos += 1
+            continue
+
+        pos += 1
+
+    return tokens
+            
+            
 
 if __name__ == "__main__":
-    lex("main.sc")
+    
+    tks = lex("main.sc")
+
+    for t in tks:
+        print("TOKEN {} AT LOCATION {} OF FILE".format(tokens[t[0]],t[2]))
---
+++ tokens.py
@@ -0,0 +1,32 @@
+# Store the valid tokens as strings
+tokens = [
+    "<IDENTIFIER>",
+    "<INTCON>",
+    "<CHARCON>",
+    "<STRINGCON>",
+    "<END>",
+    "<SPACE>",
+    "<ASSIGNMENTOPERATOR>",
+    "<UNARYOPERATOR>",
+    "<PARENTHESIS>",
+    "<LINEFEED>",
+    "<CARRAGERETURN>",
+    "<TAB>",
+    "<SEMICOLON>",
+    "<RESERVEDWORD>"
+]
+
+# Create the 'enums'
+IDENTIFIER = 0
+INTCON = 1
+CHARCON = 2
+STRINGCON = 3
+END = 4
+SPACE = 5
+ASSIGNMENTOPERATOR = 6
+UNARYOPERATOR = 7
+PARENTHESIS = 8
+LINEFEED = 9
+CARRAGERETURN = 10
+TAB = 11
+SEMICOLON = 12
