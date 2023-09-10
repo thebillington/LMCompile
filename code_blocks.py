a396d1dc759e4365e50fa86589b14cbcf391cbf4
Billy Rebecchi <billy.rebecchi@googlemail.com>
2023-09-10 21:01:46 +0100

    Add to dynamic string loading, currently broken as can't return to previous execution after string print

--- code_blocks.py
+++ code_blocks.py
@@ -1,3 +1,4 @@
 data_entry = "{id}\tDAT\t{value}\n"
 print_integer = "\tlda {id}\n\tout\n"
-print_character = "\tlda {id}\n\totc\n"
+print_string = "\tLDA\t{inst}\n\tSTA\tsp\nBRA\tsp\n"
+print_string_function_call = "sp\tLDA\t0\n\tBRZ\tsp_end\n\tOTC\n\tLDA\tsp\n\tADD\tone\n\tSTA\tsp\n\tBRA\tsp\nsp_end\tLDA\t0\n"
--- code_generator.py
+++ code_generator.py
@@ -5,14 +5,19 @@ class CodeGenerator:
 
     def __init__(self, program):
         self.program = program
-        self.symbol_table = {}
-        self.output = ""
+        self.symbol_table = {"one":1}
+        self.output = "p_start\tLDA\t0\n"
+        self.has_string_print = False
 
     def generate(self):
         for statement in self.program:
             self.generate_statement(statement)
 
-        self.generate_data_table()
+        if not self.has_string_print:
+            self.output += code_blocks.print_string_function_call
+            self.has_string_print = True
+
+        self.output = "\tBRA\tp_start\n" + self.generate_data_table() + self.output
         return self.output
     
     def generate_statement(self, statement):
@@ -27,20 +32,23 @@ class CodeGenerator:
         self.generate_statement(statement.child)
 
     def generate_data_table(self):
+        symbols = ""
         for k in self.symbol_table.keys():
-            self.output += code_blocks.data_entry.format(id=k, value=self.symbol_table[k])
-
+            symbols += code_blocks.data_entry.format(id=k, value=self.symbol_table[k])
+        return symbols
+        
     def get_symbol(self, value):
         existing_symbol = self.check_symbol_table(value)
         if existing_symbol:
             return existing_symbol
         
-        symbol_key = f'id{len(self.symbol_table)}'
+        symbol_no = len(self.symbol_table)
+        symbol_key = f'id{symbol_no}'
         if isinstance(value, int):
             self.symbol_table[symbol_key] = value
         if isinstance(value, str):
             self.symbol_table[symbol_key] = ord(value)
-        return symbol_key
+        return (symbol_key, symbol_no)
 
     def check_symbol_table(self, value):
         for k in self.symbol_table.keys():
@@ -50,14 +58,16 @@ class CodeGenerator:
 
     def generate_print_i(self, statement):
         value = statement.child
-        symbol = self.get_symbol(value)
+        symbol, symbol_no = self.get_symbol(value)
         self.output += code_blocks.print_integer.format(id=symbol)
 
     def generate_print_c(self, statement):
-        value = statement.child
+        value = list(statement.child + "\0")
+        symbol, symbol_no = self.get_symbol(value.pop(0))
         for char in value:
-            symbol = self.get_symbol(char)
-            self.output += code_blocks.print_character.format(id=symbol)
+            self.get_symbol(char)
+        instruction, inst_no = self.get_symbol(int(f'5{symbol_no+1:02d}'))
+        self.output += code_blocks.print_string.format(inst=instruction)
 
     def generate_print_c_string(self, statement):
         return
