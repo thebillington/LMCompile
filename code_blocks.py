66afb8b33f23e2b644eb45cd270009ce6207613f
Billy Rebecchi <billy.rebecchi@googlemail.com>
2023-09-16 23:16:10 +0100

    Add callbacks to calling code for string print routine

--- code_blocks.py
+++ code_blocks.py
@@ -1,4 +1,17 @@
-data_entry = "{id}\tDAT\t{value}\n"
-print_integer = "\tlda {id}\n\tout\n"
-print_string = "\tLDA\t{inst}\n\tSTA\tsp\nBRA\tsp\n"
-print_string_function_call = "sp\tLDA\t0\n\tBRZ\tsp_end\n\tOTC\n\tLDA\tsp\n\tADD\tone\n\tSTA\tsp\n\tBRA\tsp\nsp_end\tLDA\t0\n"
+class IntegerPrint:
+    def __init__(self, data_id):
+        self.data_id = data_id
+    def compile(self):
+        return f"\tLDA\t{self.data_id}\n\tout\n"
+    
+class StringPrint:
+    def __init__(self, calling_inst_id):
+        self.calling_inst_id = calling_inst_id
+        self.code_lines = 5
+    def compile(self, return_inst_id):
+        return f"\tLDA\t{self.calling_inst_id}\n\tSTA\tsp\n\tLDA\t{return_inst_id}\n\tSTA\tsp_end\n\tBRA\tsp\n"
+    
+class StringPrintRoutine:
+    @staticmethod
+    def compile():
+        return "sp\tLDA\t0\n\tBRZ\tsp_end\n\tOTC\n\tLDA\tsp\n\tADD\tone\n\tSTA\tsp\n\tBRA\tsp\nsp_end\tLDA\t0\n"
--- code_generator.py
+++ code_generator.py
@@ -5,20 +5,38 @@ class CodeGenerator:
 
     def __init__(self, program):
         self.program = program
-        self.symbol_table = {"one":1}
-        self.output = "p_start\tLDA\t0\n"
+        self.symbol_table = {"one":"\tDAT\t1"}
+        self.blocks = []
         self.has_string_print = False
+        self.calling_blocks = 0
 
     def generate(self):
         for statement in self.program:
             self.generate_statement(statement)
 
-        if not self.has_string_print:
-            self.output += code_blocks.print_string_function_call
-            self.has_string_print = True
+        output = \
+            "\tBRA\tp_start\n" \
+            + self.generate_data_table() \
+            + "{instruction_callbacks}" \
+            + "p_start\tLDA\t0\n"
+        
+        callback_table = {}
+        for block in self.blocks:
+            if isinstance(block, code_blocks.IntegerPrint):
+                output += block.compile()
+            elif isinstance(block, code_blocks.StringPrint):
+                callback_no = len(callback_table)
+                callback_id = f"cb{callback_no}"
+                n = output.count("\n")
+                callback_table[callback_id] = f'\tBRA\t{n+self.calling_blocks+block.code_lines}'
+                output += block.compile(f"{callback_id}")
 
-        self.output = "\tBRA\tp_start\n" + self.generate_data_table() + self.output
-        return self.output
+        output += "\tHLT\n"
+
+        if self.has_string_print:
+            output += code_blocks.StringPrintRoutine.compile()
+
+        return output.format(instruction_callbacks=self.generate_callback_table(callback_table))
     
     def generate_statement(self, statement):
         if not hasattr(statement, "child"):
@@ -27,27 +45,26 @@ class CodeGenerator:
         if isinstance(statement, nodes.PrintI):
             self.generate_print_i(statement.child)
         if isinstance(statement, nodes.PrintC):
-            self.generate_print_c(statement.child)
+            self.generate_print_s(statement.child)
 
         self.generate_statement(statement.child)
 
     def generate_data_table(self):
-        symbols = ""
+        data_table = ""
         for k in self.symbol_table.keys():
-            symbols += code_blocks.data_entry.format(id=k, value=self.symbol_table[k])
-        return symbols
+            data_table += f"{k}{self.symbol_table[k]}\n"
+        return data_table
+    
+    def generate_callback_table(self, callback_table):
+        c_table = ""
+        for k in callback_table.keys():
+            c_table += f"{k}{callback_table[k]}\n"
+        return c_table
         
     def get_symbol(self, value):
-        existing_symbol = self.check_symbol_table(value)
-        if existing_symbol:
-            return existing_symbol
-        
         symbol_no = len(self.symbol_table)
         symbol_key = f'id{symbol_no}'
-        if isinstance(value, int):
-            self.symbol_table[symbol_key] = value
-        if isinstance(value, str):
-            self.symbol_table[symbol_key] = ord(value)
+        self.symbol_table[symbol_key] = value
         return (symbol_key, symbol_no)
 
     def check_symbol_table(self, value):
@@ -58,16 +75,19 @@ class CodeGenerator:
 
     def generate_print_i(self, statement):
         value = statement.child
-        symbol, symbol_no = self.get_symbol(value)
-        self.output += code_blocks.print_integer.format(id=symbol)
+        symbol, symbol_no = self.get_symbol(f"\tDAT\t{value}")
+        self.blocks.append(code_blocks.IntegerPrint(symbol))
 
-    def generate_print_c(self, statement):
+    def generate_print_s(self, statement):
+        self.has_string_print = True
+        self.calling_blocks += 1
         value = list(statement.child + "\0")
-        symbol, symbol_no = self.get_symbol(value.pop(0))
+        first_char = ord(value.pop(0))
+        symbol, symbol_no = self.get_symbol(f"\tDAT     {first_char}") # 5 spaces here because for some reason a \t breaks??????
         for char in value:
-            self.get_symbol(char)
-        instruction, inst_no = self.get_symbol(int(f'5{symbol_no+1:02d}'))
-        self.output += code_blocks.print_string.format(inst=instruction)
+            self.get_symbol(f"\tDAT\t{ord(char)}")
+        inst_symbol, _ = self.get_symbol(f"\tLDA\t{symbol_no+1}")
+        self.blocks.append(code_blocks.StringPrint(inst_symbol))
 
     def generate_print_c_string(self, statement):
         return
